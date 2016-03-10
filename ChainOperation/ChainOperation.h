1e5931fe812f0a618d997894b6cf91096efc10a6
Tino Heth <th@t-no.de>
2016-03-10 16:53:41 +0100

    Initial Commit

---
+++ ChainOperation/ChainOperation.h
@@ -0,0 +1,19 @@
+//
+//  ChainOperation.h
+//  ChainOperation
+//
+//  Created by Tino Heth on 10.03.16.
+//  Copyright © 2016 t-no. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChainOperation.
+FOUNDATION_EXPORT double ChainOperationVersionNumber;
+
+//! Project version string for ChainOperation.
+FOUNDATION_EXPORT const unsigned char ChainOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChainOperation/PublicHeader.h>
+
+
